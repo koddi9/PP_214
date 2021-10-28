1ec10c42b571010b6cf5184ca108c59a2c70681d
koddi9 <mors.1.course@gmail.com>
2021-10-28 03:16:24 +0300

    Final

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,17 +1,31 @@
 package koschei.config;
 
+
+import koschei.models.Egg6;
 import koschei.models.Island2;
+import koschei.models.Needle7;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "koschei")
+//@PropertySource("classpath:spring-types-of-wiring.properties")
 public class AppConfig {
+
 
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+    @Bean
+    public static Egg6 getEgg(Needle7 needle){
+        return new Egg6(needle);
+    }
+
+
+
 }
